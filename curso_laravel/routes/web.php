e42a141cf66563ae2128b688f30ae883da114ddd
pedro <pedropapoti@gmail.com>
2023-09-19 15:14:13 -0300

    add routes

--- curso_laravel/routes/web.php
+++ curso_laravel/routes/web.php
@@ -16,3 +16,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::any('/any',function(){
+    return "Permite todo tipo de acesso http (put, delete, get, post )";
+});
+
+Route::match(['get','post'],'/match', function(){
+    return "Permite apenas acessos de finidos";
+});
+
+Route::get('/produto/{id}/', function($id){
+    return " O id do produto é: " .$id;
+
+});
+
+
+Route::any('/exemplo/{id}/{cat}',function($id, $cat){
+    return "id do produto : " .$id. "<br>" . "A categoria é " .$cat;
+    
+});
